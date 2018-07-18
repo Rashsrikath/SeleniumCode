f65dc8ed528caae51e226837b6f975b54a5c2288
Training_B8C.07.19 <Training_B8C.07.19@BDC8-DX-431JX52.dir.svc.accenture.com>
2018-07-18 12:04:35 +0530

    commit

---
+++ SeleniumWebDriver/src/test/java/testing/Test1.java
@@ -0,0 +1,49 @@
+package testing;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Parameters;
+import org.testng.annotations.Test;
+
+public class Test1 {
+    @Parameters("browser")
+	@Test
+	public void testlogic( String value)
+	{
+    	WebDriver driver=null;
+    	System.out.println(value);
+        if (value.equals("chrome")) {
+			
+        	System.setProperty("Webdriver.chrome.driver","C:\\eclipse\\chromedriver.exe");
+    		driver=new ChromeDriver();
+    		
+		} else if(value.equals("firefox")){
+			
+			System.setProperty("Webdriver.geckodriver.driver","C:\\eclipse\\geckodriver.exe");
+    		driver=new FirefoxDriver();
+    		
+		}
+        else {
+        	System.out.println("No driver Found");
+        }
+		
+		driver.get("https://github.com/login");
+		driver.findElement(By.xpath("//*[@id='login_field']")).sendKeys("Rashsrikath");
+		driver.findElement(By.xpath("//*[@id='password']")).sendKeys("Rash@1992");
+		driver.findElement(By.xpath("//*[@value='Sign in']")).click();
+		String title=driver.getTitle();
+		System.out.println(title);
+		Assert.assertEquals(title, "GitHub");
+		System.out.println("Continue");
+		if (title.contains("GitHub")) {
+			 System.out.println("Successfully logged In");
+			 System.out.println("Successfully logged In");
+		} else {
+			System.out.println("Failed to login to browser");
+
+		}
+	}
+
+}
